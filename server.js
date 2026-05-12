89b7cdd939a11f85da9024aaecbc2a6f74808da2
forbelskym22 <mforbelak@gmail.com>
2026-05-12 18:51:16 +0200

    chore: change default port to 4000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 const NOTES_ROOT = path.join(__dirname, 'Maturita', 'ústní')
 
 app.use(express.static(path.join(__dirname, 'public')))
